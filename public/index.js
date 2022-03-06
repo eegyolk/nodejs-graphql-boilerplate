f7c1101e057d89ff87deef439ee09bf78af63eb8
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-06 17:23:33 +0800

    Should not use destructor

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ const express = require('express'),
   app = express(),
   compression = require('compression');
 
-const appBootstrap = require('../bootstrap/app'),
-  config = appBootstrap.config;
+const bootstrapApp = require('../bootstrap/app');
+config = bootstrapApp.config;
 
 app.use(compression({ threshold: 0 }));
 app.disable('x-powered-by');
 
-appBootstrap.extendApp({ app });
+bootstrapApp.extendApp({ app });
 
 if (config.app.env !== 'test') {
   app.listen(config.app.port, () => {
