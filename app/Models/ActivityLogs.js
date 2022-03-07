f78a766aaab5c573c66f8f5773de4807595b3568
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-07 11:47:44 +0800

    Set id value to uuid

--- app/Models/ActivityLogs.js
+++ app/Models/ActivityLogs.js
@@ -1,8 +1,17 @@
 const { Model } = require('objection');
+const { v4: uuidv4 } = require('uuid');
 
 class ActivityLogs extends Model {
   static get tableName() {
     return 'activity_logs';
+  }
+
+  async $beforeInsert(queryContext) {
+    const parent = super.$beforeInsert(queryContext);
+
+    return Promise.resolve(parent).then(() => {
+      this.id = uuidv4();
+    });
   }
 
   static get relationMappings() {}
