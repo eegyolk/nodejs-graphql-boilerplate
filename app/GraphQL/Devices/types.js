ce4c760324fedfdcd8fb3353b791d32f30119426
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-07 16:06:39 +0800

    Added user field

--- app/GraphQL/Devices/types.js
+++ app/GraphQL/Devices/types.js
@@ -5,11 +5,18 @@ const {
   GraphQLInt,
 } = require('graphql');
 
+const { usersType } = require('../Users/types');
+
 const devicesType = new GraphQLObjectType({
   name: 'Devices',
   fields: {
     id: { type: new GraphQLNonNull(GraphQLInt) },
     user_id: { type: new GraphQLNonNull(GraphQLInt) },
+    user: {
+      type: new GraphQLNonNull(usersType),
+      resolve: (source, args, { loaders }) =>
+        loaders.users.load(source.user_id),
+    },
     ip_address: { type: new GraphQLNonNull(GraphQLString) },
     user_agent: { type: new GraphQLNonNull(GraphQLString) },
     user_agent_md5: { type: new GraphQLNonNull(GraphQLString) },
