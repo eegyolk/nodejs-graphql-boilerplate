1c7545ee325d19542e5a2842a8689290fbaac7ea
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-07 16:56:17 +0800

    Implemented users and roles loader

--- app/GraphQL/UserRoles/types.js
+++ app/GraphQL/UserRoles/types.js
@@ -5,12 +5,25 @@ const {
   GraphQLInt,
 } = require('graphql');
 
+const { rolesType } = require('../Roles/types');
+const { usersType } = require('../Users/types');
+
 const userRolesType = new GraphQLObjectType({
   name: 'UserRoles',
   fields: {
     id: { type: new GraphQLNonNull(GraphQLInt) },
     user_id: { type: new GraphQLNonNull(GraphQLInt) },
+    user: {
+      type: new GraphQLNonNull(usersType),
+      resolve: (source, args, { loaders }) =>
+        loaders.users.load(source.user_id),
+    },
     role_id: { type: new GraphQLNonNull(GraphQLInt) },
+    roles: {
+      type: new GraphQLNonNull(rolesType),
+      resolve: (source, args, { loaders }) =>
+        loaders.roles.load(source.role_id),
+    },
     created_at: { type: new GraphQLNonNull(GraphQLString) },
     updated_at: { type: new GraphQLNonNull(GraphQLString) },
     deleted_at: { type: GraphQLString },
