5cdb34bc1f15ea47ad9a08c65ddb6c5e091fddf6
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-03-31 21:47:12 +0800

    Seed data for house_rules

---
+++ database/seeds/202203270000_insert_table_house_rules.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('house_rules').del();
+  await knex('house_rules').insert([
+    {
+      code: 'KIDS_ALLOWED',
+      label: 'Visitors Allowed',
+      description: '',
+    },
+    {
+      code: 'VISITORS_ALLOWED',
+      label: 'Visitors Allowed',
+      description: '',
+    },
+    {
+      code: 'PETS_ALLOWED',
+      label: 'Pets Allowed',
+      description: '',
+    },
+    {
+      code: 'SMOKING_ALLOWED',
+      label: 'Smoking Allowed',
+      description: '',
+    },
+  ]);
+};
