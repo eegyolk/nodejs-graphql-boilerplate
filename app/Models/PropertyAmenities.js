3bd328a44b60a3743390dd8b61b29e0c0febfa74
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-02 10:24:09 +0800

    Removed to a different one

--- app/Models/PropertyAmenities.js
+++
@@ -1,9 +0,0 @@
-const { Model } = require('objection');
-
-class PropertyAmenities extends Model {
-  static get tableName() {
-    return 'property_amenities';
-  }
-}
-
-module.exports = PropertyAmenities;
